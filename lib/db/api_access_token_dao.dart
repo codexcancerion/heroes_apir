af70caf713f4d43d9f37d7df7cfab5abbbb37ce0
codexcancerion <130748253+codexcancerion@users.noreply.github.com>
2025-04-24 21:47:25 +0800

    Update api_access_token_dao.dart

--- lib/db/api_access_token_dao.dart
+++ lib/db/api_access_token_dao.dart
@@ -14,12 +14,12 @@ class ApiAccessTokenDao {
   }
 
   Future<String?> getApiAccessToken() async {
-    final db = await _dbManager.database;
-    final result = await db.query('apiAccessToken', limit: 1);
-    if (result.isNotEmpty) {
-      return result.first['token'] as String?;
-    }
-    return null;
+    // final db = await _dbManager.database;
+    // final result = await db.query('apiAccessToken', limit: 1);
+    //if (result.isNotEmpty) {
+     // return result.first['token'] as String?;
+    // }
+    return "eae25af25d8ef0fbf045fd97217bd209";
   }
 
   Future<void> deleteApiAccessToken() async {
